4d934492faec436ab02861d81ee086de519e879e
Will Boyd <will@codersblock.com>
2018-06-01 15:37:52 -0400

    Much refactoring

--- demos/floating-bubbles/my.js
+++ demos/floating-bubbles/my.js
@@ -1,131 +1,153 @@
 // plugin
 Matter.use('matter-wrap');
 
-const COLORS = ['#c5f6fa', '#d0ebff', '#d3f9d8'];
-const MIN_BODY_RADIUS = 50;
-const MAX_BODY_RADIUS = 100;
-const FRICTION_AIR = 0.03;
-const SCROLL_VELOCITY = 0.025;
-const BODIES_PER_PIXEL = 0.00002;
+let floatingBubbles = {
+	// customizable options (passed into init function)
+    options: {
+        canvasSelector: '',
+        colors: ['#c5f6fa', '#d0ebff', '#d3f9d8'],
+        minBodyRadius: 50,
+        maxBodyRadius: 100,
+        airFriction: 0.03,
+        scrollVelocity: 0.025,
+        bodiesPerPixel: 0.00002
+    },
 
-const SCROLL_DELAY = 200;
-const RESIZE_DELAY = 400;
+	// throttling intervals (in ms)
+	scrollDelay: 100,
+	resizeDelay: 400,
 
-let engine;
-let render;
-let runner;
+	// throttling variables and timeouts
+	lastScrollTop: undefined,
+	scrollTimeout: undefined,
+	resizeTimeout: undefined,
 
-let viewportWidth;
-let viewportHeight;
-let bodies;
-let lastScrollTop;
-let scrollTimeout;
-let resizeTimeout;
+	// Matter.js objects
+	engine: undefined,
+	render: undefined,
+	runner: undefined,
+	bodies: undefined,
 
-function init() {
-    viewportWidth = document.documentElement.clientWidth;
-    viewportHeight = document.documentElement.clientHeight;
+	// kicks things off
+	init(options) {
+		// override default options with incoming options
+		this.options = Object.assign(this.options, options);
 
-    lastScrollTop = document.documentElement.scrollTop;
+		let viewportWidth = document.documentElement.clientWidth;
+		let viewportHeight = document.documentElement.clientHeight;
 
-    scrollTimeout = null;
-    resizeTimeout = null;
+		this.lastScrollTop = document.documentElement.scrollTop;
+		this.scrollTimeout = null;
+		this.resizeTimeout = null;
+	
+		// engine
+		this.engine = Matter.Engine.create();
+		this.engine.world.gravity.y = 0;
+	
+		// render
+		this.render = Matter.Render.create({
+			canvas: document.querySelector(this.options.canvasSelector),
+			engine: this.engine,
+			options: {
+				width: viewportWidth,
+				height: viewportHeight,
+				wireframes: false,
+				background: 'transparent'
+			}
+		});
+		Matter.Render.run(this.render);
+	
+		// runner
+		this.runner = Matter.Runner.create();
+		Matter.Runner.run(this.runner, this.engine);
+	
+		// bodies
+		this.bodies = [];
+		let totalBodies = Math.round(viewportWidth * viewportHeight * this.options.bodiesPerPixel);
+		for (let i = 0; i <= totalBodies; i++) {
+			let body = this.createBody(viewportWidth, viewportHeight);
+			this.bodies.push(body);
+		}
+		Matter.World.add(this.engine.world, this.bodies);
 
-    // engine
-    engine = Matter.Engine.create();
-    engine.world.gravity.y = 0;
+		// events
+		window.addEventListener('scroll', this.onScrollThrottled.bind(this));
+		window.addEventListener('resize', this.onResizeThrottled.bind(this));
+	},
+	
+	// stop all the things
+	shutdown() {
+		Matter.Engine.clear(this.engine);
+		Matter.Render.stop(this.render);
+		Matter.Runner.stop(this.runner);
 
-    // render
-    render = Matter.Render.create({
-        element: document.body,
-        engine: engine,
-        options: {
-            width: viewportWidth,
-            height: viewportHeight,
-            wireframes: false,
-            background: 'transparent'
-        }
+		window.removeEventListener('scroll', this.onScrollThrottled);
+		window.removeEventListener('resize', this.onResizeThrottled);
+	},
+	
+	// random number generator
+	rand(min, max) {
+		return Math.random() * (max - min) + min;
+	},
+	
+	// create body with some random parameters
+	createBody(viewportWidth, viewportHeight) {
+		let x = this.rand(0, viewportWidth);
+		let y = this.rand(0, viewportHeight);
+		let radius = this.rand(this.options.minBodyRadius, this.options.maxBodyRadius)
+		let color = this.options.colors[this.bodies.length % this.options.colors.length];
+	
+		return Matter.Bodies.circle(x, y, radius, {
+			frictionAir: this.options.airFriction,
+			render: { fillStyle: color },
+			plugin: {
+				wrap: {
+					min: { x: 0, y: 0 },
+					max: { x: viewportWidth, y: viewportHeight }
+				}
+			}
+		});
+	},
+	
+	// enforces throttling of scroll handler
+	onScrollThrottled() {
+		if (!this.scrollTimeout) {
+			this.scrollTimeout = setTimeout(this.onScroll.bind(this), this.scrollDelay);
+		}
+	},
+	
+	// applies velocity to bodies based on scrolling with some randomness
+	onScroll() {
+		this.scrollTimeout = null;
+	
+		let delta = (this.lastScrollTop - document.documentElement.scrollTop) * this.options.scrollVelocity;
+	
+		this.bodies.forEach((body) => {
+			Matter.Body.setVelocity(body, {
+				x: body.velocity.x + delta * this.rand(-0.5, 0.5),
+				y: body.velocity.y + delta * this.rand(0.5, 1.5)
+			});
+		});
+	
+		this.lastScrollTop = document.documentElement.scrollTop;
+	},
+	
+	// enforces throttling of resize handler
+	onResizeThrottled() {
+		if (!this.resizeTimeout) {
+			this.resizeTimeout = setTimeout(this.onResize.bind(this), this.resizeDelay);
+		}
+	},
+	
+	// restart everything with the new viewport size
+	onResize() {
+		this.shutdown();
+		this.init();
+	}
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	Object.create(floatingBubbles).init({
+        canvasSelector: '#bg'
     });
-    Matter.Render.run(render);
-
-    // runner
-    runner = Matter.Runner.create();
-    Matter.Runner.run(runner, engine);
-
-    // bodies
-    bodies = [];
-    let totalBodies = Math.round(viewportWidth * viewportHeight * BODIES_PER_PIXEL);
-    for (let i = 0; i <= totalBodies; i++) {
-        bodies.push(createBody());
-    }
-    Matter.World.add(engine.world, bodies);
-}
-
-function shutdown() {
-    render.canvas.remove();
-    Matter.Engine.clear(engine);
-    Matter.Render.stop(render);
-    Matter.Runner.stop(runner);
-}
-
-function rand(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function createBody() {
-    let x = rand(0, viewportWidth);
-    let y = rand(0, viewportHeight);
-    let radius = rand(MIN_BODY_RADIUS, MAX_BODY_RADIUS)
-    let color = COLORS[bodies.length % COLORS.length];
-
-    return Matter.Bodies.circle(x, y, radius, {
-        frictionAir: FRICTION_AIR,
-        render: { fillStyle: color },
-        plugin: {
-            wrap: {
-                min: { x: 0, y: 0 },
-                max: { x: viewportWidth, y: viewportHeight }
-            }
-        }
-    });
-}
-
-function onScrollThrottled() {
-    if (!scrollTimeout) {
-        scrollTimeout = setTimeout(onScroll, SCROLL_DELAY);
-    }
-}
-
-function onScroll() {
-    scrollTimeout = null;
-
-    let delta = (lastScrollTop - document.documentElement.scrollTop) * SCROLL_VELOCITY;
-
-    bodies.forEach((body) => {
-        Matter.Body.setVelocity(body, {
-            x: body.velocity.x + delta * rand(-0.5, 0.5),
-            y: body.velocity.y + delta * rand(0.5, 1.5)
-        });
-    });
-
-    lastScrollTop = document.documentElement.scrollTop;
-}
-
-function onResizeThrottled() {
-    if (!resizeTimeout) {
-        resizeTimeout = setTimeout(onResize, RESIZE_DELAY);
-    }
-}
-
-function onResize() {
-    shutdown();
-    init();
-}
-
-window.addEventListener('load', function() {
-    init();
-
-    // wire events
-    window.addEventListener('scroll', onScrollThrottled);
-    window.addEventListener('resize', onResizeThrottled);
 });
